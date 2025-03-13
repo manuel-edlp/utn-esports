73145b3c94bbc02c67c1ceda4fbb05677c5d4996
Manuel Morullo <manuelmorullo@hotmail.com>
2025-03-13 02:45:56 -0300

    feat: agrego url y vista para la seccion faq (preguntas frecuentes)

--- src/core/views.py
+++ src/core/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home/home.html'
+
+class Faq(TemplateView):
+    template_name = 'faq/faq.html'
