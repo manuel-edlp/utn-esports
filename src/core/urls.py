f2463c059d7392bfd7842c2ac9b60ea06b1d0152
Manuel Morullo <manuelmorullo@hotmail.com>
2025-03-15 01:54:29 -0300

    feat: agrego url para iniciar sesion, cerrar sesion, y para el home del jugador

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('faq/', views.Faq.as_view(), name='faq'),
     path('sponsors/', views.Sponsors.as_view(), name='sponsors'),
     path('register/', views.RegistroView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    # Jugador
+    path('player/home/', views.PlayerHomeView.as_view(), name='player_home'),
 ]
