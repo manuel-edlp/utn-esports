9df9fa8083542cdd45e5d7cf79c9dc09edc55359
Manuel Morullo <manuelmorullo@hotmail.com>
2025-03-14 21:16:25 -0300

    feat: agrego url para que se registren los participantes

--- src/core/urls.py
+++ src/core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('faq/', views.Faq.as_view(), name='faq'),
     path('sponsors/', views.Sponsors.as_view(), name='sponsors'),
+    path('register/', views.RegistroView.as_view(), name='register'),
 ]
