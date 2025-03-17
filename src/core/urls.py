4e4b63a834fcda0ad6174dad16a0be4563af860f
Manuel Morullo <manuelmorullo@hotmail.com>
2025-03-17 02:29:55 -0300

    feat: agrego url para el reglamento

--- src/core/urls.py
+++ src/core/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('faq/', views.Faq.as_view(), name='faq'),
+    path('reglamento/', views.Reglamento.as_view(), name='reglamento'),
     path('sponsors/', views.Sponsors.as_view(), name='sponsors'),
     path('register/', views.RegistroView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
