d2f9faf661db87b578942d66b7cd995b38cc388c
Manuel Morullo <manuelmorullo@hotmail.com>
2025-03-23 21:04:21 -0300

    feat: creo filtro para separar el riot id

--- src/core/templatetags/filters.py
+++ src/core/templatetags/filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter
 def basename(value):
     return os.path.basename(value)
+
+@register.filter
+def before_hash(value):
+    """Devuelve la parte de la cadena antes del '#'."""
+    return value.split("#")[0] if value else ""
