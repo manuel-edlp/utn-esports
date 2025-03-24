55ae0d6c9591d4d8dfb580889caa4b3ac4556370
Manuel Morullo <manuelmorullo@hotmail.com>
2025-03-24 13:42:51 -0300

    fix: corrijo lenguaje code

--- src/utn_esports/settings.py
+++ src/utn_esports/settings.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'es_AR'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
